cb885bedaa6a05deba0550e214b87ba13f8c34e9
Parvat Raj Singh <parvat.raj2@gmail.com>
2026-02-09 15:19:26 +0530

    feat: complete gps demo

--- examples/gps_neo7m_demo/gps_neo7m_demo.ino
+++ examples/gps_neo7m_demo/gps_neo7m_demo.ino
@@ -1,28 +1,4 @@
 /**
- * Solduino GPS NEO-7M Sensor-to-Chain Demo (Scaffold)
- */
-
-#include <WiFi.h>
-#include <solduino.h>
-
-const char* ssid = "YOUR_WIFI_SSID";
-const char* password = "YOUR_WIFI_PASSWORD";
-const String RPC_ENDPOINT = SOLDUINO_DEVNET_RPC;
-
-Solduino solduino;
-RpcClient rpcClient(RPC_ENDPOINT);
-
-void setup() {
-    Serial.begin(115200);
-    solduino.begin();
-    WiFi.begin(ssid, password);
-    rpcClient.begin();
-}
-
-void loop() {
-    delay(1000);
-}
-/**
  * Solduino GPS (NEO-7M) Sensor-to-Chain Demo
  *
  * Reads GPS coordinates from a NEO-7M module over UART and periodically pushes
